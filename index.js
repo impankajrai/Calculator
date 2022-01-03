6c3d63015e635e60278540702d26270e1978034e
PANKAJ RAI <87197538+impankajrai@users.noreply.github.com>
2022-01-03 15:45:54 +0530

    unminify the javascript file

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const year=document.getElementById("year");const LowerTextBox=document.getElementById("lowerInput");const UperTextBox=document.getElementById("uperInput");function btnClick(event){if(LowerTextBox.value=="0"){LowerTextBox.value=""}
-let btnVal=LowerTextBox.value.concat(event.value);LowerTextBox.value=btnVal}
-function btnEqual(){UperTextBox.value=LowerTextBox.value;LowerTextBox.value=eval(UperTextBox.value)}
+const year=document.getElementById("year");
+const LowerTextBox=document.getElementById("lowerInput");
+const UperTextBox=document.getElementById("uperInput");
+function btnClick(event){
+      
+      if(LowerTextBox.value=="0"){
+        LowerTextBox.value=""
+        }
+      let btnVal=LowerTextBox.value.concat(event.value);
+      LowerTextBox.value=btnVal
+}
+
+function btnEqual(){
+  UperTextBox.value=LowerTextBox.value;
+  LowerTextBox.value=eval(UperTextBox.value)
+}
 year.innerText=new Date().getFullYear();
